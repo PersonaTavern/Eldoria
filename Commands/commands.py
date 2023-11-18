e704714e094b49ce16a7ced03dc75a72fe20bf75
capt-eq <151294347+capt-eq@users.noreply.github.com>
2023-11-18 10:39:31 +0800

    Create commands.py

---
+++ Commands/commands.py
@@ -0,0 +1,26 @@
+# commands.py
+@bot.command(name='attack')
+async def attack(ctx, target):
+    # Implement command logic to initiate combat
+    pass
+
+@bot.command(name='use')
+async def use_item(ctx, item):
+    # Implement command logic to use an item
+    pass
+
+@bot.command(name='inventory')
+async def show_inventory(ctx):
+    # Implement command logic to display the player's inventory
+    pass
+
+@bot.command(name='stats')
+async def show_stats(ctx):
+    # Implement command logic to display player statistics
+    pass
+
+@bot.command(name='quests')
+async def show_quests(ctx):
+    # Implement command logic to display available quests
+    pass
+  
