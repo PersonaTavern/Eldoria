45f4ae7ad962d04d4954768b59ff50c0e4b39215
capt-eq <151294347+capt-eq@users.noreply.github.com>
2023-11-21 13:58:13 +0800

    Update main.py
    Added another sample main.py 
    This should be the first file to be executed.

--- main.py
+++ main.py
@@ -7,3 +7,39 @@ bot = commands.Bot(command_prefix='!')
 async def on_message(message):
     # Implement main game loop logic here
     await bot.process_commands(message)
+
+    
+## Sample Code from https://builtin.com/software-engineering-perspectives/discord-bot-python
+# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+import discord
+
+# GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+bot = discord.Client()
+
+# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
+@bot.event
+async def on_ready():
+	# CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
+	guild_count = 0
+
+	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+	for guild in bot.guilds:
+		# PRINT THE SERVER'S ID AND NAME.
+		print(f"- {guild.id} (name: {guild.name})")
+
+		# INCREMENTS THE GUILD COUNTER.
+		guild_count = guild_count + 1
+
+	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+@bot.event
+async def on_message(message):
+	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	if message.content == "hello":
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("hey dirtbag")
+
+# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+bot.run("NzA0OTcwNzUyNzQyNjUzOTYz.Xqk5ww.1U_-WdW4aeGWCNF7bOJkLAu_2TM")
