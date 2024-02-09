6f8bdda35a8ea40c40734f6b8be3b42a221721e3
Captain <151294347+capt-eq@users.noreply.github.com>
2024-02-09 21:18:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,26 @@ from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+"""
+From Discord.py API page
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('my token goes here')
+
+
+"""
+
+
+
 # Enable intents
 intents = discord.Intents.default()
 intents.guilds = True  # Enables GUILDS intent
